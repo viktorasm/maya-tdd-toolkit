889590f3cec8fe5b09088f132a6932df95909a43
Viktoras Makauskas <viktoras@neglostyti>
2016-02-10 07:59:43 +0200

    Revert "failed attempt for sockets"
    This reverts commit f025b69b19a07af4f30c06fd6154cfc2d9e4157d.

--- mayatdd/server.py
+++ mayatdd/server.py
@@ -7,33 +7,11 @@ import SocketServer
 import threading
 import json
 import urllib2
-import struct
-
-class JsonStream:
-    def __init__(self,sock):
-        '''
-        :param sock: socket.socket 
-        '''
-        print repr(sock)
-        self.sock = sock
-        self.rfile = self.sock.makefile('rb', 1024)
-        self.wfile = self.sock.makefile('wb', 1024)
-        
-    def sendJson(self,doc):
-        #self.wfile.writelines([json.dumps(doc)])
-        self.wfile.write("asdf\n")
-        
-        
-    def readJson(self):
-        #return json.dumps(self.rfile.readline())
-        return self.rfile.readline()
-        
 
 class Client:
     def __init__(self,host,port):
-        import socket
-        self.sock = socket.create_connection((host,port))
-        self.jsonStream = JsonStream(self.sock)
+        self.timeout = 300
+        self.endpoint = "http://{0}:{1}".format(host,port)
         
     def send(self,jsonDictionary):
         '''
@@ -41,12 +19,9 @@ class Client:
         
         no particular error checking is done as we trust our server in a way.
         '''
-        print "client: sending json"
-        self.jsonStream.sendJson(jsonDictionary)
-        print "client: json sent"
-        
-        print "client: reading json"
-        return self.jsonStream.readJson()
+        data = json.dumps(jsonDictionary)
+        response = urllib2.urlopen(self.endpoint, data=data, timeout=self.timeout)
+        return json.loads(response.read())
         
 
 class Server:
@@ -54,23 +29,19 @@ class Server:
         self.port = port
     
     def run(self, requestHandlerMethod):
+        class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+            instance = None
         
-        class JsonStreamHandler(SocketServer.StreamRequestHandler):
-            def handle(self):
-                print "server handling"
-                print self.rfile.readline()
-                print "input read"
-                self.wfile.writelines(["you fail"])
-                self.wfile.flush()
-                return
-                print "handling stuff"
-                h = JsonStream(self.request)
-                data = h.readJson()
-                print "data received",data
-                result = requestHandlerMethod(data)
-                h.sendJson(result)
-                
-        self.instance = SocketServer.TCPServer(("", self.port), JsonStreamHandler) 
+            def do_GET(self):
+                self.wfile.write("maya tdd server\n")
+        
+            def do_POST(self):
+                request = self.rfile.read(int(self.headers['Content-Length']))
+                request = json.loads(request)
+                result = requestHandlerMethod(request)
+                self.wfile.write(json.dumps(result))
+            
+        self.instance = SocketServer.TCPServer(("", self.port), RequestHandler) 
         threading.Thread(target=self.instance.serve_forever).start()
     
     def stop(self):

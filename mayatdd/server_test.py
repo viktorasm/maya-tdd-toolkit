547faaa1aac3c70ebc94fc4a24c01bab241166f5
Viktoras Makauskas <viktoras@neglostyti.com>
2018-08-08 16:27:28 +0300

    converting to pipenv

--- mayatdd/server_test.py
+++ mayatdd/server_test.py
@@ -2,29 +2,28 @@ import unittest
 import server
 
 
-
 class ServerTest(unittest.TestCase):
     def setUp(self):
-        self.port = 6777 
+        self.port = 6777
         self.server = server.Server(self.port)
-        
+
     def testConnect(self):
         '''
         validate that server-client connection is happening and that handler is installed correctly
         '''
+
         def fakeHandler(request):
-            if request=={'a':'b'}:
-                return {'c':'d'}
+            if request == {'a': 'b'}:
+                return {'c': 'd'}
             else:
                 return 'not the right thing received'
-            
+
         self.server.run(fakeHandler)
 
         for i in range(100):
-            client = server.Client("127.0.0.1",self.port)
-            result = client.send({'a':'b'})
-            self.assertEquals(result, {'c':'d'})
-        
+            client = server.Client("127.0.0.1", self.port)
+            result = client.send({'a': 'b'})
+            self.assertEquals(result, {'c': 'd'})
+
     def tearDown(self):
         self.server.stop()
-        
--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="mayatdd",
     version="0.0.2",
     packages=["mayatdd"],
-    include_package_data = True,
+    include_package_data=True,
     install_requires=[],
-
     dependency_links=[],
 )
